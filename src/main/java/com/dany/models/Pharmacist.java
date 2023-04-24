c159a3215cc9510338eea2a1cf4ae3e3df874676
kdany25 <kabadany25@gmail.com>
2023-04-24 18:00:03 +0200

    chore: refactored codes

--- src/main/java/com/dany/models/Pharmacist.java
+++ src/main/java/com/dany/models/Pharmacist.java
@@ -1,24 +1,61 @@
 package com.dany.models;
 
 import com.dany.database.DataStore;
+import com.dany.enums.Gender;
+import com.dany.enums.UserRole;
 import com.dany.helpers.ApiResponse;
 
 public class Pharmacist extends User {
     @Override
     public ApiResponse<User> register() throws Exception {
-        validatePassword();
+        validateUserDetails();
         encryptPassword();
         DataStore.addUser(this);
-        User user = DataStore.findUser(getEmail());
+        User user = DataStore.getUserByPhone(getPhone());
         return new ApiResponse<>("registered a pharmacist successfully", user);
     }
 
+    private void validateUserDetails() throws Exception {
+        validateUserName();
+        validatePhone();
+        validatePassword();
+        validateGender();
+        validateRole();
+    }
+    
+    private void validateUserName() throws Exception {
+        if (getUserName() == null || getUserName().isEmpty()) {
+            throw new Exception("Username must be provided");
+        }
+    }
+    
+    private void validatePhone() throws Exception {
+        if (getPhone() == null || getPhone().isEmpty()) {
+            throw new Exception("Phone number must be provided");
+        }
+    }
+    
     private void validatePassword() throws Exception {
         String password = getPassword();
         if (password.length() < 9 || password.length() > 10) {
             throw new Exception("Password must have a length of 9-10 characters");
         }
     }
+    
+    private void validateGender() throws Exception {
+        Gender gender = getGender();
+        if (gender != Gender.MALE && gender != Gender.FEMALE) {
+            throw new Exception("Gender must be either MALE or FEMALE");
+        }
+    }
+    
+    private void validateRole() throws Exception {
+        UserRole role = getRole();
+        if (role != UserRole.PATIENT && role != UserRole.PHYSICIAN && role != UserRole.PHARMACIST) {
+            throw new Exception("Role must be either PATIENT, PHYSICIAN or PHARMACIST");
+        }
+    }
+    
 
     @Override
     public void createUser(User user) {
@@ -29,5 +66,7 @@ public class Pharmacist extends User {
         setEmail(user.getEmail());
         setPassword(user.getPassword());
         setGender(user.getGender());
+        setUserName(user.getUserName());
+        setPhone(user.getPhone());
     }
 }
--- src/main/java/com/dany/models/User.java
+++ src/main/java/com/dany/models/User.java
@@ -27,11 +27,12 @@ public abstract class User {
     private String lastName;
     @NonNull
     private String userName;
-    @Expose(serialize = false)
-    private String password;
-    private Gender gender;
     @NonNull
     private String phone;
+    @Expose(serialize = false)
+    private String password;
+    @NonNull
+    private Gender gender;
     private Integer age;
     private UserRole role;
 
@@ -57,14 +58,53 @@ public abstract class User {
         return decryptedPasswordBuilder.toString();
     }
 
-    public ApiResponse<String> login(String email, String password) throws Exception {
-        User user = DataStore.findUser(email);
+    public ApiResponse<String> login(String usernameOrEmailOrPhone, String password) throws Exception {
+        User user = null;
+        String errorMessage = "Invalid credentials";
+        if (isUsername(usernameOrEmailOrPhone)) {
+            user = DataStore.getUserByUsername(usernameOrEmailOrPhone);
+            errorMessage = "Invalid username or password";
+        } else if (isEmail(usernameOrEmailOrPhone)) {
+            user = DataStore.getUserByEmail(usernameOrEmailOrPhone);
+            errorMessage = "Invalid email or password";
+        } else if (isPhone(usernameOrEmailOrPhone)) {
+            user = DataStore.getUserByPhone(usernameOrEmailOrPhone);
+            errorMessage = "Invalid phone number or password";
+        }
+
         if (user == null) {
-            throw new AuthenticationException("Invalid credentials");
+            throw new AuthenticationException(errorMessage);
         }
+
         if (!password.equals(user.decryptPassword())) {
-            throw new AuthenticationException("Invalid credentials");
+            throw new AuthenticationException(errorMessage);
         }
+
         return new ApiResponse<>("You have logged in successfully", GenerateJwt.generateJwt(user));
     }
+
+    private boolean isUsername(String value) {
+        if (value.matches("^[a-zA-Z]\\w*$")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isEmail(String value) {
+        if (value.matches("^\\S+@\\S+\\.\\S+$")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isPhone(String value) {
+        if (value.matches("^\\d{10}$")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
